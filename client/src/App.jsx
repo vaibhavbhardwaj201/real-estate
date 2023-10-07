80760e17e11fff1100ec8cc026993f2dbea0bcc1
Vaibhav Bhardwaj <vaibhavbhardwaj1230@gmail.com>
2023-10-07 17:40:00 +0300

    Initial setup

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="bg-red-400 text-3xl">
+        Click on the Vite and React logos to learn more
+      </h1>
+    </>
+  )
+}
+
+export default App
