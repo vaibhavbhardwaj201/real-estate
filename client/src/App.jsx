30c39b93df7c81b080579f90e355afc62fddce78
Vaibhav Bhardwaj <vaibhavbhardwaj1230@gmail.com>
2023-10-07 21:31:39 +0300

    Add: pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,29 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom'
+
 import './App.css'
+import SignIn from './pages/SignIn'
+import Home from './pages/Home'
+import SignUp from './pages/SignUp'
+import Profile from './pages/Profile'
+import About from './pages/About'
 
 function App() {
 
   return (
     <>
-      <h1 className="bg-red-400 text-3xl">
-        Click on the Vite and React logos to learn more
-      </h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
